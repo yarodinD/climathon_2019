c99561da8a2276198ebdf611c0b68e192f320f6b
Sven Sander <Sven.Sander@tipico.com>
2019-10-25 18:14:49 +0200

    initial checkin -2

---
+++ src/test/java/de/climathon/extremeweather/mawarning/MawarningApplicationTests.java
@@ -0,0 +1,13 @@
+package de.climathon.extremeweather.mawarning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MawarningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
