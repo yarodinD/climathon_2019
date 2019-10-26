c25feaebbf1b6b8b096a3062a4b82f4334260bad
Sven Sander <Sven.Sander@tipico.com>
2019-10-26 17:46:50 +0200

    add static data for analysis

---
+++ src/main/java/de/climathon/extremeweather/mawarning/domain/model/WarnLevel.java
@@ -0,0 +1,5 @@
+package de.climathon.extremeweather.mawarning.domain.model;
+
+public enum WarnLevel {
+    NO_WARNING, VERY_LOW, LOW, MEDIUM, HIGH, UNKNOWN
+}
---
+++ src/main/java/de/climathon/extremeweather/mawarning/domain/model/staticdata/StaticData.java
@@ -0,0 +1,61 @@
+package de.climathon.extremeweather.mawarning.domain.model.staticdata;
+
+
+import de.climathon.extremeweather.mawarning.domain.model.WarnLevel;
+import org.springframework.util.Assert;
+
+public class StaticData {
+
+    // temperature to humidity relations; values indicate felt temperatures;
+    // danger levels are defined by ranges on those values
+    public static final Integer[][] TEMP_HUMIDITY_LEVELS = {
+            {27, 28, 29, 30, 31, 32, 34, 35, 37, 39, 41, 43, 46, 48, 51, 54, 57},
+            {27, 28, 29, 30, 32, 33, 35, 37, 39, 41, 43, 46, 49, 51, 54, 57, 99},
+            {27, 28, 30, 31, 33, 34, 36, 38, 41, 43, 46, 49, 52, 55, 58, 99, 99},
+            {28, 29, 30, 32, 34, 36, 38, 40, 43, 46, 48, 52, 55, 59, 99, 99, 99},
+            {28, 29, 31, 33, 35, 37, 40, 42, 45, 48, 51, 55, 59, 99, 99, 99, 99},
+            {28, 30, 32, 34, 36, 39, 41, 44, 48, 51, 55, 59, 99, 99, 99, 99, 99},
+            {29, 31, 33, 35, 38, 40, 43, 47, 50, 54, 58, 99, 99, 99, 99, 99, 99},
+            {29, 31, 34, 36, 39, 42, 46, 49, 53, 58, 99, 99, 99, 99, 99, 99, 99},
+            {30, 32, 35, 38, 41, 44, 48, 52, 57, 99, 99, 99, 99, 99, 99, 99, 99},
+            {30, 33, 36, 39, 43, 47, 51, 55, 99, 99, 99, 99, 99, 99, 99, 99, 99},
+            {31, 34, 37, 41, 45, 49, 54, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99},
+            {31, 35, 38, 42, 47, 51, 57, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99},
+            {32, 36, 40, 44, 49, 54, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99}
+    };
+
+    public static WarnLevel calculateWarnLevelByTempAndHumidity(final Double temperature, final Double humidity) {
+        Assert.notNull(temperature, "Temperature must not be null");
+        Assert.notNull(humidity, "Humidity must not be null");
+
+        int roundedTemp = Double.valueOf(Math.floor(temperature)).intValue();
+        int roundedHumidity = (Double.valueOf(Math.floor(humidity)).intValue() / 5) * 5;
+
+        if ((roundedTemp < 27) || (roundedHumidity < 40)) {
+            return WarnLevel.NO_WARNING;
+        }
+
+        if (roundedTemp > 43) {
+            return WarnLevel.HIGH;
+        }
+
+        Integer feltTemp = TEMP_HUMIDITY_LEVELS[roundedHumidity - 40][roundedTemp - 27];
+
+        if (feltTemp >= 27 && feltTemp <=32) {
+            return WarnLevel.VERY_LOW;
+        }
+
+        if (feltTemp > 32 && feltTemp <= 40) {
+            return WarnLevel.LOW;
+        }
+
+        if (feltTemp > 40 && feltTemp <=54) {
+            return WarnLevel.MEDIUM;
+        }
+
+        if (feltTemp > 54) {
+            return WarnLevel.HIGH;
+        }
+        return WarnLevel.UNKNOWN;
+    }
+}
